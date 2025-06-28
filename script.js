5c0d3195caf3e6a4bf8497377e94dba01e151045
@AbinoSin <3abino@gmail.com>
2025-06-28 20:44:25 +0530

    Big Changes(28-06-25)

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const userSeq = [];
 let started = false;
 let level = 0;
 
-let test = document.querySelector(".btn");
+let test = document.querySelectorAll(".btn");
 
 document
   .querySelector(".preinfo p button")
@@ -20,12 +20,43 @@ document
 function levelUp() {
   level++;
   document.querySelector(".preinfo p").innerHTML = `LEVEL <b>${level}</b>`;
-  btnFlash(test);
+  btnFlashAll(test);
 }
 
-function btnFlash(pBtn) {
-  pBtn.classList.add("flash");
+function btnFlashAll(pBtn) {
+  for (item of pBtn) {
+    item.classList.add("flash");
+  }
   setTimeout(() => {
-    pBtn.classList.remove("flash");
-  }, 250);
+    pBtn[0].classList.remove("flash");
+  }, 500);
+  setTimeout(() => {
+    pBtn[2].classList.remove("flash");
+  }, 1000);
+  setTimeout(() => {
+    pBtn[3].classList.remove("flash");
+  }, 1500);
+  setTimeout(() => {
+    pBtn[1].classList.remove("flash");
+  }, 2000);
+  setTimeout(() => {
+    pBtn.forEach((element) => {
+      element.classList.add("flash");
+    });
+  }, 2500);
+  setTimeout(() => {
+    pBtn.forEach((element) => {
+      element.classList.remove("flash");
+    });
+  }, 3000);
+  setTimeout(() => {
+    pBtn.forEach((element) => {
+      element.classList.add("flash");
+    });
+  }, 3500);
+  setTimeout(() => {
+    pBtn.forEach((element) => {
+      element.classList.remove("flash");
+    });
+  }, 4000);
 }
