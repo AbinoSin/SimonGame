07b246e22cc2a16231364b8f7650410a7587b615
@AbinoSin <3abino@gmail.com>
2025-06-28 20:04:48 +0530

    Big Changes(28-06-25)

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const gameSeq = [];
 const userSeq = [];
 
 let started = false;
-let level = 1;
+let level = 0;
+
+let test = document.querySelector(".btn");
 
 document
   .querySelector(".preinfo p button")
@@ -11,5 +13,19 @@ document
       started = true;
       console.log("game is started");
       document.querySelector(".preinfo p").innerHTML = "Game is started !!";
+      levelUp();
     }
   });
+
+function levelUp() {
+  level++;
+  document.querySelector(".preinfo p").innerHTML = `LEVEL <b>${level}</b>`;
+  btnFlash(test);
+}
+
+function btnFlash(pBtn) {
+  pBtn.classList.add("flash");
+  setTimeout(() => {
+    pBtn.classList.remove("flash");
+  }, 250);
+}
